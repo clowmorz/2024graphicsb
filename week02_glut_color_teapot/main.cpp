f54a2811f30eb70fc20fe4a0f62d68761b23f5dc
clowmorz <112918647+clowmorz@users.noreply.github.com>
2024-02-26 16:45:18 +0800

    week02

---
+++ week02_glut_color_teapot/main.cpp
@@ -0,0 +1,24 @@
+#include <GL/glut.h>
+
+void display()
+{
+    glColor3f(247/255.0, 180/255.0, 171/255.0);
+    glutSolidTeapot(0.3);
+
+    glColor3f(200/255.0, 25/255.0, 200/255.0);
+    glutSolidTeapot(0.2);
+
+    glutSwapBuffers();
+
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+     glutCreateWindow("week02_glut_color_teapot");
+
+     glutDisplayFunc(display);
+
+      glutMainLoop();
+}
